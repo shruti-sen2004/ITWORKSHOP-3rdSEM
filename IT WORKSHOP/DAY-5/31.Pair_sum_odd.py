0d36661c7e42ad7216e1c350be8ea57fa4f93d18
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2023-10-12 21:10:31 +0530

    Delete IT WORKSHOP/DAY-5/31.Pair_sum_odd.py

--- IT WORKSHOP/DAY-5/31.Pair_sum_odd.py
+++
@@ -1,7 +0,0 @@
-numbers = eval(input("ENTER LIST: "))
-odd_numbers = [x for x in numbers if x % 2 == 1]
-result = []
-for i in range(len(odd_numbers)):
-    for j in range(i+1, len(odd_numbers)):
-        result.append((odd_numbers[i], odd_numbers[j]))
-print(f"The distinct pairs from the list {numbers} are: {result}")
