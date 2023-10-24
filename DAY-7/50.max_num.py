144ecd9b29e7eaf89a2821680d495ed5ba979614
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2023-10-24 09:08:59 +0530

    Delete DAY-7 directory

--- DAY-7/50.max_num.py
+++
@@ -1,6 +0,0 @@
-numbers = [int(x) for x in eval(input("Enter a list of numbers: "))]
-
-maximum_value = max(numbers, key=lambda x: x)
-
-
-print(f"The maximum value in the list is: {maximum_value}")
