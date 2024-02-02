7280375c09efcfa1a07cdbc4127d0c3aae2bcf5f
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-02 23:21:19 +0530

    Update 6.number_display.py

--- IT WORKSHOP/DAY-1/6.number_display.py
+++ IT WORKSHOP/DAY-1/6.number_display.py
@@ -1,6 +1,9 @@
-n= int(input("ENTER A NUMBER: "))
-
+n= (input("ENTER A NUMBER: "))
+#M-1
 for i in range(4,0,-1):
-   
     n= n%(10**i)
     print(n)
+
+#M-2
+for i in range(len(n),0,-1):
+    print(n[:i:])
