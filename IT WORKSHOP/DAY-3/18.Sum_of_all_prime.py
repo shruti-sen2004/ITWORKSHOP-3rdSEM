3bf847ee724cd4925a2c1f2b6e630f4451ac95d8
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-03 00:15:05 +0530

    Update 18.Sum_of_all_prime.py

--- IT WORKSHOP/DAY-3/18.Sum_of_all_prime.py
+++ IT WORKSHOP/DAY-3/18.Sum_of_all_prime.py
@@ -1,11 +1,11 @@
-last_num = int(input("ENETR THE RANGE: "))
+l_num= int(input("ENTER the END NUMBER: "))
 sum =0
-for num in range(2, last_num +1):
-    i =2
-    for i in range(2, num):
-        if(int(num % i)==0):
-            i = num 
-            break;
+for num in range(0,l_num+1):
+    i=1
+    for i in range(2,num):
+        if(int(num%i)==0):
+            i=num
+            break
     if i is not num:
         sum += num
 print("\nTHE SUM OF ALL PRIME NUMBERS: ",sum)
