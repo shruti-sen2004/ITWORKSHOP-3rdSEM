32ff0314314cf8ab5f959775215d263f0d95df65
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-05 20:43:03 +0530

    Update 34.add_elem_tup.py

--- IT WORKSHOP/DAY-5/34.add_elem_tup.py
+++ IT WORKSHOP/DAY-5/34.add_elem_tup.py
@@ -1,12 +1,7 @@
-def summation(test_tup):
-    test = list(test_tup)
-    count = 0
- 
-    for i in test:
-        count += i
-    return count
- 
- 
+tup = eval(input("ENTER A TUPLE: "))
+n= int(input("ENTER THE NUMBER OF TERMS YOU WANT TO ADD IN THE TUPLE: "))
+for i in range(n):
+    num= int(input("ENTER THE NUMBER YOU WOULD LIKE TO ADD: "))
+    tup = tup + (num,)
 
-test_tup = eval(input("ENTER A TUPLE: "))
-print(summation(test_tup))
+print("NEW TUPLE: ",tup)
