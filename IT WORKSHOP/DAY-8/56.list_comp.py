fea1bcc778071866ca8daee919d18fbd2a6bbc1b
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-05 23:35:05 +0530

    Update 56.list_comp.py

--- IT WORKSHOP/DAY-8/56.list_comp.py
+++ IT WORKSHOP/DAY-8/56.list_comp.py
@@ -2,5 +2,5 @@ import random
 l= int(input("ENTER A LOWER RANGE: "))
 u =int(input("ENTER A UPPER RANGE: "))
 n= int(input("ENTER THE NUMBER OF TERMS: "))
-res = [random.randrange(l,u) for i in range(n)]
-print ("Random number list is : " +  str(res))
+res = [random.randint(l,u) for i in range(n)]
+print ("Random number list is : ",res)
