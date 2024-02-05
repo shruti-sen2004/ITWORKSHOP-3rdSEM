e165421027b631e6fe305b66702e822dc36edbf4
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-05 23:12:20 +0530

    Update prime.py

--- IT WORKSHOP/DAY-8/prime.py
+++ IT WORKSHOP/DAY-8/prime.py
@@ -1,24 +1,16 @@
 def prime_or_not(num):
-    count =0
+    f =0 
     i=1
-    print("Factors are: ",end=" ")
     while i<=num:
         if num%i ==0:
-            print(i,end=" ")
-            count +=1
+            f +=1
         i +=1
-    if count ==2:
-            print("\nNumber is prime")
+    if f==2:
+        return True
     else: 
-            print("\nNumber is not prime")
+        return False
 
 def prime_range(a,b):
-    print("Prime numbers bertween",a,"and",b,"are: ",end=" ")
-    for num in range(a,b+1):
-        i=2
-        for i in range(2,num):
-            if(int(num % i)==0):
-                i= num
-                break;
-        if i is not num:
-            print(num, end=" ")
+    for i in range(a,b+1):
+        if prime_or_not(i)== True:
+            print(i,end=" ")
