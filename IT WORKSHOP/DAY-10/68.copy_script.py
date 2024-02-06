ce730b88047e04f22676c6b8115e055fd2fab969
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-06 18:49:47 +0530

    Update 68.copy_script.py

--- IT WORKSHOP/DAY-10/68.copy_script.py
+++ IT WORKSHOP/DAY-10/68.copy_script.py
@@ -1,9 +1,10 @@
-f_name= input("ENTER THE FILE NAME: ")
-d_name= input("ENTER THE FILE NAME WHERE YOU WANT TO COPY THE FILE: ")
+f_name= input("ENTER THE NAME OF THE FILE: ")
 
-with open(f_name,'r') as source :
-    with open(d_name,'w') as new_file:
-        for line in source:
-            if not line.lstrip().startswith('#'):
-                new_file.write(line)
+with open(f_name,'r') as file:
+    content = file.readlines()
+    with open('new_script.py','w') as file:
+        for i in range(0,len(content)):
+            if content[i][0]!="#":
+                file.write(content[i])
+
 print("CONTENT COPIED!!")
