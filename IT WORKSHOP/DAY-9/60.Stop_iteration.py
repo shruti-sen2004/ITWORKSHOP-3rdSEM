9e8e27d7e68481b2da9f5a1ea2f4d67eb39e57aa
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-06 13:31:55 +0530

    Update 60.Stop_iteration.py

--- IT WORKSHOP/DAY-9/60.Stop_iteration.py
+++ IT WORKSHOP/DAY-9/60.Stop_iteration.py
@@ -1,11 +1,16 @@
 import random
+count =0
 
-count = 0
-
-while True:
-    if count >= 10:
-        raise StopIteration("Displayed 10 numbers. Exiting...")
-    
-    random_number = random.randint(1, 100)
-    print("Random number:", random_number)
-    count += 1
+l= int(input("ENTER THE LOWER BOUND= "))
+u= int(input("ENTER THE UPPER BOUND= "))
+print("RANDOM NUMBERS ARE: ",end=" ")
+try:
+    while True:
+        if count>=10:
+            raise ValueError("\n10 NUMBERS HAS BEEN DISPLAYED!!")
+        
+        num = random.randint(l,u+1)
+        print(num,end=" ")
+        count +=1
+except ValueError as e:
+    print(e)
