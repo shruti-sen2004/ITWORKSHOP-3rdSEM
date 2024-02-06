178176a075cabcbe7f1b9364632e92b6e0f01250
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-06 13:16:24 +0530

    Update 58.Arithmatic_operations.py

--- IT WORKSHOP/DAY-9/58.Arithmatic_operations.py
+++ IT WORKSHOP/DAY-9/58.Arithmatic_operations.py
@@ -1,43 +1,34 @@
-def get_valid_number(prompt):
-    while True:
-        try:
-            num = int(input(prompt))
-            return num
-        except ValueError:
-            print("Invalid input. Please enter a valid number.")
+choice = "yes"
 
-while True:
+while choice.lower()=="yes":
     try:
-        num1 = get_valid_number("Enter the first number: ")
-        num2 = get_valid_number("Enter the second number: ")
+        n1= int(input("ENTER FIRST NUMBER: "))
+        n2= int(input("ENTER SECOND NUMBER: "))
 
-        print("Select an operation:")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Multiplication")
-        print("4. Division")
-        choice = input("Enter your choice (1/2/3/4): ")
-
-        if choice == '1':
-            result = num1 + num2
-            print("Result:", result)
-        elif choice == '2':
-            result = num1 - num2
-            print("Result:", result)
-        elif choice == '3':
-            result = num1 * num2
-            print("Result:", result)
-        elif choice == '4':
-            if num2 == 0:
-                print("Division by zero is not allowed.")
+        print('''SELECT OPERATION:
+                1. ADD
+                2. SUBSTRACTION
+                3. MULTIPLICATION
+                4. DIVISION ''')
+        ch= int(input("ENTER YOUR CHOICE: "))
+        if ch in [1,2,3,4]:
+            if ch==1:
+                result = n1+n2
+                print("RESULT: ",result)
+            elif ch==2:
+                result = n1-n2
+                print("RESULT: ",result)
+            elif ch==3:
+                result = n1*n2
+                print("RESULT: ",result)
+            elif ch==4:
+                if n2==0:
+                    raise ValueError("DIVISION BY ZERO IS NOT POSSIBLE!!")
+                else:
+                    result = n1/n2
+                    print("RESULT: ",result)
             else:
-                result = num1 / num2
-                print("Result:", result)
-        else:
-            print("Invalid choice. Please select 1, 2, 3, or 4.")
-    except Exception as e:
-        print("An error occurred:", e)
-
-    again = input("Do you want to perform another calculation? (yes/no): ")
-    if again.lower() != 'yes':
-        break
+                raise ValueError("ENTER CORRECT CHOICE!!")
+    except ValueError as e:
+        print(e)
+    choice= input("DO YOU WANT TO DO OTHER OPERATIONS?")
