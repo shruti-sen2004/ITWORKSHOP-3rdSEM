6c4c9cd2cc8ec2d4a7349144bb94c972fc617b6a
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-07 10:03:14 +0530

    Update 16.Armstrong.py

--- IT WORKSHOP/DAY-3/16.Armstrong.py
+++ IT WORKSHOP/DAY-3/16.Armstrong.py
@@ -1,12 +1,14 @@
-num = int(input("ENETR THE NUMBER: "))
+num = int(input("ENTER A NUMBER: "))
 sum =0
-if num >=100 and num <=999:
-    s = num
-    while s>0:
-        digit = s%10
-        sum += digit **3
-        s =s//10
+o= len(str(num))
+
+s= num
+while s>0:
+    digit = s%10
+    sum += digit**o
+    s= s//10
+
 if sum == num:
-    print("IT IS A ARMSTRONG NUMBER!!")
+    print("ARMSTRONG")
 else:
-    print("IT IS NOT A ARMSTRONG NUMBER")
+    print("NOT ARMSTRONG")
