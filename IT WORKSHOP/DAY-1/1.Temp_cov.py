b16b3a1ddbbe0b55a7fa8e8a2f773e2728dbb98d
Shruti Sen <115914670+shruti-sen2004@users.noreply.github.com>
2024-02-07 09:46:55 +0530

    Update 1.Temp_cov.py

--- IT WORKSHOP/DAY-1/1.Temp_cov.py
+++ IT WORKSHOP/DAY-1/1.Temp_cov.py
@@ -1,5 +1,5 @@
 def temp_con(temp):
-    return (18*temp/100)+32
+    return (1.8*temp)+32
 
 cel= float(input("ENTER A TEMPERATURE IN CELCIUS: "))
 print("TEMPERATURE IN FARENHEIT IS: ",temp_con(cel))
